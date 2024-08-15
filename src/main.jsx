ff14509a16ae82ca5a0f286b963c6a576f08f480
SebastianU3 <sebastian.utomo12@gmail.com>
2024-08-15 16:25:35 +0700

    teest

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ Sentry.init({
     Sentry.replayIntegration(),
   ],
   environment: "production",
+  release: "latest",
   // Performance Monitoring
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
